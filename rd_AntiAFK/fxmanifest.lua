cd53ecedc433d89d9b991bb3082aab5b5b3bb3d9
RespectDevelopment <143709835+respectdevelopment@users.noreply.github.com>
2024-01-04 18:39:11 +0100

    Fxmanifest update

--- rd_AntiAFK/fxmanifest.lua
+++ rd_AntiAFK/fxmanifest.lua
@@ -1,15 +1,18 @@
+-- {{FX Information}} --
 fx_version 'cerulean'
 game 'gta5'
 
+-- {{Resource Information}} --
 author 'Respect Development'
 description 'Anti AFK'
 version '1.0.0'
 
+-- {{Manifest}} --
 lua54 'yes'
 
 shared_scripts {
     'shared/config.lua',
-    --'@ox_lib/init.lua', -- UNMARK IT HERE, IF YOU WANT USE OXLIB NOTIFICATION
+    --'@ox_lib/init.lua', ⚠️ Unmark it here, if you want use ox_lib notification ⚠️
 }
 
 client_scripts {
